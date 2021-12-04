c8b2e85360e9a7d9318699b7d929268924f932d2
unterguk <kilian.unterguggenberger@physik.hu-berlin.de>
2021-12-04 09:36:46 +0100

    solve part II!

--- 04/day04.py
+++ 04/day04.py
@@ -32,6 +32,22 @@ def play_bingo(d_array, b_list):
                 return calculate_score(b, d)
 
 
+def find_last_winning(d_array, b_list):
+    losers = b_list
+    for d in d_array:
+        for b in losers:
+            single_draw(d, b)
+
+        old_losers = losers
+        losers = []
+        for b in old_losers:
+            if not check_victory(b):
+                losers.append(b)
+
+        if len(losers) == 0:
+            return calculate_score(old_losers[0], d)
+
+
 def test_play():
     assert play_bingo(test_draws, test_boards) == 4512
 
@@ -41,8 +57,14 @@ def test_read():
     assert (test_boards[1][0] == second_board_first_row).all()
 
 
+def test_last():
+    assert find_last_winning(test_draws, test_boards) == 1924
+
+
 draws, boards = read_bingo_data('input.txt')
 test_draws, test_boards = read_bingo_data('test_input.txt')
 
 final_score = play_bingo(draws, boards)
+final_score_last = find_last_winning(draws, boards)
 print(f'Score: {final_score}')
+print(f'Score of last winning: {final_score_last}')
