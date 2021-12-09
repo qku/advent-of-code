92a824c589784c530f524b3b607ef19fece68f25
unterguk <kilian.unterguggenberger@physik.hu-berlin.de>
2021-12-09 11:20:10 +0100

    simplify

--- 09/day09.py
+++ 09/day09.py
@@ -9,33 +9,6 @@ def pad_map(m):
     return np.pad(m, 1, constant_values=9)
 
 
-def find_minimum_values(h_map, return_index=False):
-    minima = []
-    minima_index = []
-    rows, columns = h_map.shape
-    for row in range(1, rows - 1):
-        for column in range(1, columns - 1):
-            val = h_map[row, column]
-
-            up = h_map[row - 1, column]
-            down = h_map[row + 1, column]
-            left = h_map[row, column - 1]
-            right = h_map[row, column + 1]
-
-            if val < min(up, down, left, right):
-                minima.append(val)
-                minima_index.append((row, column))
-
-    if return_index:
-        return minima_index
-    else:
-        return np.array(minima, dtype=int)
-
-
-def total_risk_value(minima):
-    return (minima + 1).sum()
-
-
 def neighbors(index):
     row, column = index
     up = (row - 1, column)
@@ -43,6 +16,25 @@ def neighbors(index):
     left = (row, column - 1)
     right = (row, column + 1)
     return [up, down, left, right]
+
+
+def find_minima(h_map):
+    minima = []
+    rows, columns = h_map.shape
+    for row in range(1, rows - 1):
+        for column in range(1, columns - 1):
+            i = (row, column)
+            if h_map[i] < min([h_map[k] for k in neighbors(i)]):
+                minima.append(i)
+    return minima
+
+
+def total_risk_value(h_map):
+    minima = find_minima(h_map)
+    tot = 0
+    for m in minima:
+        tot += h_map[m] + 1
+    return tot
 
 
 def flood_fill(node, a):
@@ -53,22 +45,22 @@ def flood_fill(node, a):
     return a
 
 
-def determine_basin_size(h_map, minimum_index):
+def determine_basin_size(h_map, minimum):
     # map identifying the basin
     #  0 -> not checked yet
     #  1 -> part of basin
     # -1 -> not part of basin
 
     basin = np.zeros_like(h_map, dtype=int)
-    basin[minimum_index] = 1
+    basin[minimum] = 1
     basin[h_map == 9] = -1
 
-    basin = flood_fill(minimum_index, basin)
+    basin = flood_fill(minimum, basin)
     return (basin > 0).sum()
 
 
 def product_of_three_largest_basins(h_map):
-    minima = find_minimum_values(h_map, return_index=True)
+    minima = find_minima(h_map)
     basin_sizes = [determine_basin_size(h_map, i) for i in minima]
     three_largest = np.sort(basin_sizes)[-3:]
     return np.prod(three_largest)
@@ -76,6 +68,5 @@ def product_of_three_largest_basins(h_map):
 
 heightmap = load_map('input.txt')
 heightmap_padded = pad_map(heightmap)
-minimum_values = find_minimum_values(heightmap_padded)
-print(f'Total risk value: {total_risk_value(minimum_values)}')
+print(f'Total risk value: {total_risk_value(heightmap_padded)}')
 print(f'Product of three largest basins: {product_of_three_largest_basins(heightmap_padded)}')
--- 09/day09_test.py
+++ 09/day09_test.py
@@ -10,7 +10,7 @@ test_map = pad_map(load_map('test_input.txt'))
 
 
 def test_find_minimum_values():
-    assert total_risk_value(find_minimum_values(test_map)) == 15
+    assert total_risk_value(test_map) == 15
 
 
 def test_flood_fill():
