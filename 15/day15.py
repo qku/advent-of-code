b8aee5132976ad9f81701cda93ee01370d1c8811
unterguk <kilian.unterguggenberger@physik.hu-berlin.de>
2021-12-15 12:59:31 +0100

    part II functional

--- 15/day15.py
+++ 15/day15.py
@@ -5,6 +5,27 @@ def read_map(f):
     return np.genfromtxt(f, delimiter=1)
 
 
+def expand(risk, n=5):
+    # tile array right and down
+    exp_risk = np.tile(risk, (n, n))
+
+    # add 1 for every tile step to right and down
+    n_orig = risk.shape[0]
+    a = np.arange(n)
+    b = np.zeros((n, n))
+    for i in range(n):
+        b[i] = a + i
+        b[i, 0] = i
+    inc = np.repeat(np.repeat(b, n_orig, axis=0), n_orig, axis=1)
+    exp_risk += inc
+
+    # wrap values back so that they are <= 9
+    to_wrap = exp_risk > 9
+    exp_risk[to_wrap] = (exp_risk[to_wrap] % 10) + 1
+
+    return exp_risk
+
+
 def neighbors(index, max_i):
     row, column = index
     pot_n = [(row, column + 1),
@@ -14,8 +35,11 @@ def neighbors(index, max_i):
     return [i for i in pot_n if max(i) < max_i and min(i) >= 0]
 
 
-def lowest_risk_dijkstra(f):
+def lowest_risk_dijkstra(f, n_expand=1):
     risk = read_map(f)
+    if n_expand > 1:
+        risk = expand(risk, n_expand)
+
     max_i = risk.shape[0]
 
     unvisited = np.ones_like(risk).astype(bool)
@@ -27,8 +51,8 @@ def lowest_risk_dijkstra(f):
     current = (0, 0)
 
     while unvisited[-1, -1]:
-        print(current)
-        print(distance[current])
+        #print(current)
+        #print(distance[current])
         # loop through unvisited neighbors
         for n in neighbors(current, max_i=max_i):
             if unvisited[n]:
@@ -40,6 +64,11 @@ def lowest_risk_dijkstra(f):
         if not unvisited.any():
             continue
 
+        # status update
+        n_unvisited = unvisited.sum()
+        frac = 1 - (n_unvisited / unvisited.size)
+        print(f'{frac:.2%}')
+
         # set node with the lowest distance to current
         unvisited_i = np.argwhere(unvisited)
         current = tuple(unvisited_i[distance[unvisited].argmin()])
@@ -47,5 +76,9 @@ def lowest_risk_dijkstra(f):
     return distance[-1, -1]
 
 
-lowest = lowest_risk_dijkstra('input.txt')
-print(f'Lowest total risk: {lowest}')
+if __name__ == '__main__':
+    lowest = lowest_risk_dijkstra('input.txt')
+    lowest_exp = lowest_risk_dijkstra('input.txt', n_expand=5)
+
+    print(f'Lowest total risk: {lowest}')
+    print(f'Lowest total risk after expansion: {lowest_exp}')
--- 15/day15_test.py
+++ 15/day15_test.py
@@ -3,3 +3,7 @@ from day15 import *
 
 def test_dijkstra():
     assert lowest_risk_dijkstra('test_input.txt') == 40
+
+
+def test_dijkstra_expanded():
+    assert lowest_risk_dijkstra('test_input.txt', n_expand=5) == 315
