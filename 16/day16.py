6ee53a5a1b53616cf0f938c390a0ddd8bea7420a
unterguk <kilian.unterguggenberger@physik.hu-berlin.de>
2021-12-17 12:12:25 +0100

    part II done!

--- 16/day16.py
+++ 16/day16.py
@@ -30,17 +30,18 @@ def parse_header(bit_data):
     return version, packet_id
 
 
-def parse_n_packets(bit_data, n=1, return_version_sum=False):
-    version_sum = 0
+def parse_n_packets(bit_data, n=1, version_sum=False):
+    content = []
     for i in range(n):
         if n != 1:
             print(f'\tparsing packet {i+1}/{n}')
         version, packet_id = parse_header(bit_data)
-        version_sum += version
+        if version_sum:
+            content.append(version)
 
         # literal
         if packet_id == 4:
-            print(f'Literal packet, version {version}')
+            print(f'literal packet, version {version}')
             literal_bits = ''
 
             # iterate over chunks
@@ -51,12 +52,14 @@ def parse_n_packets(bit_data, n=1, return_version_sum=False):
                     literal_bits += str(b)
                 del bit_data[:5]
 
-            # for now: do nothing with it
-            literal = int(literal_bits, 2)
+            if not version_sum:
+                content.append(int(literal_bits, 2))
 
         # operator
         else:
-            print(f'Operator packet, version {version}')
+            print(f'operator packet, version {version}')
+            if not version_sum:
+                content.append(f'o{packet_id}')
             length_type = bit_data[0]
             del bit_data[0]
 
@@ -66,30 +69,85 @@ def parse_n_packets(bit_data, n=1, return_version_sum=False):
                 del bit_data[:15]
                 sub_packet_data = bit_data[:sub_packet_bit_length]
 
-                version_sum += parse_all_bits(sub_packet_data,
-                                              return_version_sum=return_version_sum)
+                content.append(parse_all_bits(sub_packet_data,
+                                              version_sum=version_sum))
                 del bit_data[:sub_packet_bit_length]
 
             else:
                 n_sub_packets = bits_to_int(bit_data[:11])
                 print(f'\tcontaining {n_sub_packets} packets')
                 del bit_data[:11]
-                version_sum += parse_n_packets(bit_data, n=n_sub_packets,
-                                               return_version_sum=return_version_sum)
+                content.append(parse_n_packets(bit_data, n=n_sub_packets,
+                                               version_sum=version_sum))
 
-    if return_version_sum:
-        return version_sum
+    return content
 
 
-def parse_all_bits(bit_data, return_version_sum=False):
-    version_sum = 0
+def parse_all_bits(bit_data, version_sum=False):
+    content = []
     while bit_data:
-        version_sum += parse_n_packets(bit_data, return_version_sum=return_version_sum)
-    if return_version_sum:
-        return version_sum
+        content.append(parse_n_packets(bit_data, version_sum=version_sum))
+    return content
+
+
+def sum_up_version(content):
+    s = 0
+    for i in content:
+        if type(i) == int:
+            s += i
+        else:
+            s += sum_up_version(i)
+    return s
+
+
+def interpret_content(content):
+    literals = []
+    while content:
+        element = content.pop(0)
+
+        if type(element) == list:
+            element = interpret_content(element)[0]
+
+        if type(element) == int:
+            literals.append(element)
+
+        else:
+            operand_content = content.pop(0)
+            operands = interpret_content(operand_content)
+
+            if element == 'o0':
+                literals.append(sum(operands))
+
+            elif element == 'o1':
+                res = 1
+                for i in operands:
+                    res *= i
+                literals.append(res)
+
+            elif element == 'o2':
+                literals.append(min(operands))
+
+            elif element == 'o3':
+                literals.append(max(operands))
+
+            elif element == 'o5':
+                literals.append(int(operands[0] > operands[1]))
+
+            elif element == 'o6':
+                literals.append(int(operands[0] < operands[1]))
+
+            elif element == 'o7':
+                literals.append(int(operands[0] == operands[1]))
+
+    return literals
 
 
 if __name__ == '__main__':
     data = load_data('input.txt')
-    n_version = parse_n_packets(data, return_version_sum=True)
+    n_version = sum_up_version(parse_n_packets(data, version_sum=True))
     print(f'Version sum: {n_version}')
+
+    data = load_data('input.txt')
+    c = parse_n_packets(data, version_sum=False)
+    interpretation = interpret_content(c)
+    print(f'Message content: {interpretation[0]}')
--- 16/day16_test.py
+++ 16/day16_test.py
@@ -7,15 +7,38 @@ def test_hex_conversion():
 
 def test_version_sum_1():
     data = load_data('8A004A801A8002F478')
-    assert parse_n_packets(data, return_version_sum=True) == 16
+    assert sum_up_version(parse_n_packets(data, version_sum=True)) == 16
 
 
 def test_version_sum_operator_ltype1():
     data = load_data('EE00D40C823060')
-    assert parse_n_packets(data, return_version_sum=True) == 14
+    assert sum_up_version(parse_n_packets(data, version_sum=True)) == 14
 
 
 def test_version_sum_3():
     data = load_data('620080001611562C8802118E34')
-    assert parse_n_packets(data, return_version_sum=True) == 12
+    assert sum_up_version(parse_n_packets(data, version_sum=True)) == 12
 
+
+def test_sum():
+    data = load_data('C200B40A82')
+    assert interpret_content(parse_n_packets(data))[0] == 3
+
+
+def test_product():
+    data = load_data('04005AC33890')
+    assert interpret_content(parse_n_packets(data))[0] == 54
+
+
+def test_maximum():
+    data = load_data('CE00C43D881120')
+    assert interpret_content(parse_n_packets(data))[0] == 9
+
+def test_greater_than():
+    data = load_data('F600BC2D8F')
+    assert interpret_content(parse_n_packets(data))[0] == 0
+
+
+def test_combined():
+    data = load_data('9C0141080250320F1802104A08')
+    assert interpret_content(parse_n_packets(data))[0] == 1
