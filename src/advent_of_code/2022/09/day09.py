a0fd27729ac71e12fec693b3105331445b942a04
unterguk <kilian.unterguggenberger@physik.hu-berlin.de>
2022-12-09 08:16:31 +0100

    day 9 part II

--- src/advent_of_code/2022/09/day09.py
+++ src/advent_of_code/2022/09/day09.py
@@ -1,40 +1,46 @@
 from numpy import sign
 
+
+def follow(pos, head_pos):
+    vertical_distance = head_pos[0] - pos[0]
+    horizontal_distance = head_pos[1] - pos[1]
+    if abs(vertical_distance) > 1 or abs(horizontal_distance) > 1:
+        pos[0] += sign(vertical_distance)
+        pos[1] += sign(horizontal_distance)
+
+
 file = 'input.txt'
 # file = 'test_input.txt'
 with open(file) as f:
     steps = f.readlines()
 
-head, tail = [0, 0], [0, 0]
+# part I: 2 elements, part II: 10 elements
+n_elements = 10
+
+elements = [[0, 0] for i in range(n_elements)]
 visited = [(0, 0)]
 
 for step in steps:
     direction, n = step.strip().split()
     print(step.strip())
-    for i in range(int(n)):
-        print(f'Head: {head} - Tail: {tail}')
+    for _ in range(int(n)):
+        # print(f'Head: {head} - Tail: {tail}')
         # move head
         match direction:
             case 'D':
-                head[0] += 1
+                elements[0][0] += 1
             case 'U':
-                head[0] -= 1
+                elements[0][0] -= 1
             case 'R':
-                head[1] += 1
+                elements[0][1] += 1
             case 'L':
-                head[1] -= 1
+                elements[0][1] -= 1
 
-        # move tail
-        vertical_distance = head[0] - tail[0]
-        horizontal_distance = head[1] - tail[1]
-        if abs(vertical_distance) > 1 or abs(horizontal_distance) > 1:
-            tail[0] += sign(vertical_distance)
-            tail[1] += sign(horizontal_distance)
+        for i in range(1, n_elements):
+            follow(elements[i], elements[i-1])
 
         # save tail position
-        visited.append(tuple(tail))
-
-print(f'Head: {head} - Tail: {tail}')
+        visited.append(tuple(elements[-1]))
 
 unique_visited = set(visited)
-print(f'Tail visited {len(unique_visited)} spots.')
+print(f'Last tail element visited {len(unique_visited)} spots.')
