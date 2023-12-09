d71734f2360fc820a4afa2a1cb6b04aabca82e8e
qku <kilian.unterguggenberger@hu-berlin.de>
2023-12-09 13:53:14 +0100

    day 7 part 2

--- src/advent_of_code/2023/07/day07.py
+++ src/advent_of_code/2023/07/day07.py
@@ -1,56 +1,80 @@
 CARD_LABELS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
-def map_to_numbers(hand: str) -> list[int]:
-    return [CARD_LABELS.index(card) for card in hand]
+CARD_LABELS_J = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
+def map_to_numbers(hand: str, joker: bool = False) -> list[int]:
+    if joker:
+        return [CARD_LABELS_J.index(card) for card in hand]
+    else:
+        return [CARD_LABELS.index(card) for card in hand]
 
 
-def hand_type(hand: str) -> int:
-    hand = map_to_numbers(hand)
+def hand_type(hand: str, joker: bool = False) -> int:
+    if joker:
+        jokers = hand.count('J')
+        if jokers == 5:
+            # special case: 'JJJJJ'
+            return 6
+        hand = hand.replace('J', '')
+    else:
+        jokers = 0
     occurrences = [hand.count(i) for i in set(hand)]
-    if 5 in occurrences:
+    occurrences.sort(reverse=True)
+    if occurrences[0] + jokers == 5:
         # five of a kind
         return 6
-    if 4 in occurrences:
+    if occurrences[0] + jokers == 4:
         # four of a kind
         return 5
-    if 3 in occurrences and 2 in occurrences:
+    if occurrences[0] + occurrences[1] + jokers == 5:
         # full house
         return 4
-    if 3 in occurrences:
+    if occurrences[0] + jokers == 3:
         # three of a kind
         return 3
-    if occurrences.count(2) == 2:
+    if occurrences[0] + occurrences[1] + jokers == 4:
         # two pair
         return 2
-    if 2 in occurrences:
+    if occurrences[0] + jokers == 2:
         # one pair
         return 1
     return 0
 
 
-def key(hand_bid: tuple[str, int]) -> tuple[int, ...]:
-    hand, bid = hand_bid
-    keys = [hand_type(hand)]
-    keys += map_to_numbers(hand)
-    return tuple(keys)
+def get_key(joker: bool = False) -> callable:
+    def key(hand_bid: tuple[str, int]) -> tuple[int, ...]:
+        hand, bid = hand_bid
+        keys = [hand_type(hand, joker=joker)]
+        keys += map_to_numbers(hand, joker=joker)
+        return tuple(keys)
+    return key
 
 
-def part_1(input_file: str) -> int:
-    hands: list[tuple[str, int]] = []
+def total_winnings(hands: list[tuple[str, int]], joker: bool = False) -> int:
+    hands.sort(key=get_key(joker=joker))
+    tot = 0
+    rank = 1
+    for hand, bid in hands:
+        tot += rank * bid
+        rank += 1
+    return tot
+
+
+def read_input(input_file: str) -> list[tuple[str, int]]:
+    hands = []
     with open(input_file) as f:
         for line in f.readlines():
             splits = line.split()
             hands.append((splits[0], int(splits[1])))
-    hands.sort(key=key)
-    total_winnings = 0
-    rank = 1
-    for hand, bid in hands:
-        total_winnings += rank * bid
-        rank += 1
-    return total_winnings
+    return hands
+
+
+def part_1(input_file: str) -> int:
+    hands = read_input(input_file)
+    return total_winnings(hands)
 
 
 def part_2(input_file: str) -> int:
-    return 0
+    hands = read_input(input_file)
+    return total_winnings(hands, joker=True)
 
 
 if __name__ == '__main__':
--- src/advent_of_code/2023/07/test_day07.py
+++ src/advent_of_code/2023/07/test_day07.py
@@ -6,9 +6,16 @@ def test_map_to_numbers():
 def test_hand_type():
     assert hand_type('32T3K') == 1
     assert hand_type('T55J5') == 3
+    assert hand_type('TTT55') == 4
+    assert hand_type('KKKKK') == 6
+
+    assert hand_type('32TJK', joker=True) == 1
+    assert hand_type('T554J', joker=True) == 3
+    assert hand_type('TTJ55', joker=True) == 4
+    assert hand_type('KKKJK', joker=True) == 6
 
 def test_part_1():
     assert part_1('test_input.txt') == 6440
 
 def test_part_2():
-    assert part_2('test_input.txt') == 0
+    assert part_2('test_input.txt') == 5905
